17f1d15e64583c29012ae26e62afbab53ecc512a
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2024-10-16 16:54:46 +0700

    feat: connect to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express, { Request, Response } from 'express'
 import { config } from 'dotenv'
+import express from 'express'
+import databaseService from './services/database.services'
 config()
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
