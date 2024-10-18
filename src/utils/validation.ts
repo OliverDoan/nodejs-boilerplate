95b6642e0edb5f47f747f32612e9e244ea910fa4
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2024-10-18 10:48:06 +0700

    refactor: update errorHandler

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,16 +1,32 @@
-import { NextFunction, Request, Response } from 'express'
-import { ContextRunner } from 'express-validator'
-
+import { validationResult, ContextRunner } from 'express-validator'
+import express from 'express'
+import HTTP_STATUS from '~/constants/httpStatus'
+import { EntityError, ErrorWithStatus } from '~/models/Errors'
 export const validate = (validations: ContextRunner[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    // sequential processing, stops running validations chain if one fails.
+  return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    // Run all validations
     for (const validation of validations) {
-      const result = await validation.run(req)
-      if (!result.isEmpty()) {
-        return res.status(400).json({ errors: result.mapped() })
-      }
+      await validation.run(req)
     }
 
-    next()
+    // Collect all errors after running validations
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next()
+    }
+
+    // Initialize EntityError with an empty errors object
+    const entityError = new EntityError({ errors: {} })
+    const errorsObject = errors.mapped()
+
+    for (const key in errorsObject) {
+      const { msg } = errorsObject[key]
+      if (msg instanceof ErrorWithStatus && msg.status !== HTTP_STATUS.UNPROCESSABLE_ENTITY) {
+        return next(msg)
+      }
+      entityError.errors[key] = errorsObject[key]
+    }
+
+    next(entityError)
   }
 }
