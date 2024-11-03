8d77e9a91eb11409dba5a486bd3bd35c7256e269
Doan Viet Thuan <thuan.dv@tasco.com.vn>
2024-11-03 15:41:06 +0700

    feat: init folder upload

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,15 @@ import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { envConfig } from './constants/config'
 import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 config()
 databaseService.connect()
 
 const app = express()
 app.use(express.json())
 const port = envConfig.port || 3000
+
+initFolder()
 
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
