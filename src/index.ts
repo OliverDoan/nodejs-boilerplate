25efe3992a9caacdb28ea235b16c614dca6c33c3
OliverDoan <doanvietthuan1005@gmail.com>
2025-05-29 17:16:28 +0700

    feat(auth): #1 - implement basic login endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express, { Request, Response } from 'express'
 import { config } from 'dotenv'
+import express from 'express'
+import usersRouter from '~/routes/users.routes'
 config()
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+app.use(express.json())
+app.use('/users', usersRouter)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,1 +1,8 @@
-export {}
+import { Router } from 'express'
+import { loginController } from '~/controllers/users.controllers'
+import { loginValidator } from '~/middlewares/users.middlewares'
+const usersRouter = Router()
+
+usersRouter.post('/login', loginValidator, loginController)
+
+export default usersRouter
